e76d87466cf4a9f6b9fd4d79a915f8fa13845d30
DenyStark <denyskrolevetskyi@gmail.com>
2018-10-31 09:38:30 +0200

    Add Basic tutorial

---
+++ Tutorials/1-theBasic.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.17;
+
+contract Basic {
+	string private name;
+	uint private age;
+	
+	function setName(string newName) public {
+		name = newName;
+	}
+	function getName() public constant returns (string) {
+		return name;
+	}
+	
+	function setAge(uint newAge) public {
+		age = newAge;
+	}
+	function getAge() public constant returns (uint) {
+		return age;
+	}
+}
